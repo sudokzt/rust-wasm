e5b05bcf853080852c3bc042b31a2167c28f187a
kaaazu <wato9902@gmail.com>
2021-01-23 15:21:34 +0900

    feat: use crate

--- src/assets/js/wasm.js
+++ src/assets/js/wasm.js
@@ -1,6 +1,17 @@
-const wasm = "../target/wasm32-unknown-unknown/release/wasm_entry.wasm";
+const imports = {
+  env: {
+    date_now: Date.now,
+  },
+};
 
+const wasm = "../target/wasm32-unknown-unknown/release/wasm_entry.wasm";
+const toUint32 = (num) => num >>> 0;
 fetch(wasm)
   .then((res) => res.arrayBuffer())
-  .then((bytes) => WebAssembly.instantiate(bytes, {}))
-  .then((results) => console.log(results.instance.exports.add(1, 2)));
+  .then((bytes) => WebAssembly.instantiate(bytes, imports))
+  .then((results) => {
+    const { add, get_timestamp, rand } = results.instance.exports;
+    console.log(add(1, 2));
+    console.log(get_timestamp());
+    console.log(toUint32(rand()));
+  });
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,28 @@
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
+
+// signature: rust understands another lang functions
+extern "C" {
+    fn date_now() -> f64;
+}
+
+#[no_mangle]
+pub fn get_timestamp() -> f64 {
+    unsafe { date_now() }
+}
+
+extern crate tinymt;
+use tinymt::tinymt32;
+
+#[no_mangle]
+pub fn rand() -> u32 {
+    // TinyMT という乱数生成方式
+    let param = tinymt32::Param {
+        mat1: 0x8F7011EE,
+        mat2: 0xFC78FF1F,
+        tmat: 0x3793fdff,
+    };
+    let seed = 1;
+    tinymt32::from_seed(param, seed).gen()
+}
